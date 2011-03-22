849a7e6b3fffcfd44f818ff59f64781858d4d98b
baldzar@gmail.com <baldzar@gmail.com@aa6cf039-9e87-f74f-3bf4-c4349377eac6>
2011-03-22 01:52:57 +0000

    homography_estimator::get_subset & check_sub_set

--- trunk/ucv/lib_ucv/include/baldzarika/ucv/homography.h
+++ trunk/ucv/lib_ucv/include/baldzarika/ucv/homography.h
@@ -22,75 +22,145 @@ namespace baldzarika { namespace ucv {
 			typedef std::vector<feature_point_t> feature_points_t;
 			typedef std::pair<std::size_t, std::size_t> match_t;
 			typedef std::vector<match_t> matches_t;
-			typedef nublas::matrix< T > matrix_t;
-			
-			homography_estimator(feature_points_t const &ops, feature_points_t const &ips, matches_t const &oim, matrix_t &hm)
+			typedef nublas::matrix<value_t, nublas::row_major> matrix_t;
+
+			typedef boost::uniform_int<boost::uint32_t> uint32_dist_t;
+			typedef boost::variate_generator<boost::mt19937&, uint32_dist_t> uint32_rng_t;
+						
+			homography_estimator(feature_points_t const &ops, feature_points_t const &ips, matches_t const &oim)
 				: m_ops(ops)
 				, m_ips(ips)
 				, m_oim(oim)
-				, m_hm(hm)
+				, m_rng()
+				, m_rng_gen(m_rng, uint32_dist_t(0,m_oim.size()-1))
 			{
 			}
 
-			bool operator()()
+			template < typename AS >
+			bool operator()(nublas::matrix< value_t, nublas::row_major, AS> &hm)
 			{
-				if(m_oim.size()<=4)
+				if(m_oim.size()<4)
 					return false;
+
+				value_t min_median=std::numeric_limits<value_t>::max();
 				matrix_t models(3,3);
 				
 				std::vector<value_t> err(m_oim.size());
-				std::vector<point2_t> ms1(m_oim.size()), ms2(m_oim.size());
-				for(std::size_t m=0;m<m_oim.size();++m)
-				{
-					ms1[m]=m_ops[m_oim[m].first];
-					ms2[m]=m_ips[m_oim[m].second];
-				}
+				std::vector<point2_t> ms1(4), ms2(4);
 
-				std::size_t n_iters=round<std::size_t>(
-					log(detail::constants::one<T>()-CONFIDENCE)/
-					log(detail::constants::one<T>()-pow(detail::constants::one<T>()-OUTLIER_RATIO, T(m_oim.size())))
-				);
-				n_iters=std::min<std::size_t>( std::max<std::size_t>(n_iters, 3), MAX_ITERS);
+
+				std::size_t n_iters=m_oim.size()==4?
+					1:
+					round<std::size_t>(
+						log(detail::constants::one<T>()-CONFIDENCE)/
+						log(detail::constants::one<T>()-pow(detail::constants::one<T>()-OUTLIER_RATIO, value_t(4)))
+					);
+
+				if(m_oim.size()==4)
+				{
+					
+					for(std::size_t m=0;m<m_oim.size();++m)
+					{
+						ms1[m]=m_ops[m_oim[m].first];
+						ms2[m]=m_ips[m_oim[m].second];
+					}
+				}
+				
+				n_iters=std::min<std::size_t>( n_iters, MAX_ITERS);
 
 				for(std::size_t it=0;it<n_iters;++it)
 				{
-					if(!run_kernel(ms1, ms2, models))
-						continue;
-					/*
-					for( i = 0; i < nmodels; i++ )
+					if(m_oim.size()>4)
 					{
-						CvMat model_i;
-						cvGetRows( models, &model_i, i*modelSize.height, (i+1)*modelSize.height );
-						computeReprojError( m1, m2, &model_i, err );
-						icvSortDistances( err->data.i, count, 0 );
-
-						double median = count % 2 != 0 ?
-							err->data.fl[count/2] : (err->data.fl[count/2-1] + err->data.fl[count/2])*0.5;
-
-						if( median < minMedian )
+						if(!get_sub_set(ms1,ms2, 300))
 						{
-							minMedian = median;
-							cvCopy( &model_i, model );
+							if(!it) return false;
+							break;
 						}
 					}
-					*/
+					if(!run_kernel(ms1, ms2, models))
+						continue;
+					compute_reproj_error(models, err);
+					std::sort(err.begin(), err.end(), std::less<value_t>());
+					value_t median=m_oim.size()%2!=0?
+						err[m_oim.size()/2]:
+						(err[m_oim.size()/2-1]+err[m_oim.size()/2])*detail::constants::half<value_t>();
+					if(median<min_median)
+					{
+						min_median=median;
+						hm=models;
+					}
 				}
 
 				/*
-				if( minMedian < DBL_MAX )
+				if(min_median<std::numeric_limits<value_t>::max())
 				{
-					sigma = 2.5*1.4826*(1 + 5./(count - modelPoints))*sqrt(minMedian);
+					static value_t _coeff_1=2.5*1.4826;
+					value_t sigma=_coeff_1*(detail::constants::one<value_t>()+value_t(5)/(count - modelPoints))*sqrt(minMedian);
 					sigma = MAX( sigma, 0.001 );
 
 					count = findInliers( m1, m2, model, err, mask, sigma );
 					result = count >= modelPoints;
 				}
 				*/
-
 				return true;
 			}
 
 		protected:
+			bool get_sub_set(std::vector<point2_t> &ms1, std::vector<point2_t> &ms2, std::size_t ma)
+			{
+				boost::uint32_t idx[4];
+				std::size_t it=0, i=0;
+				for(; it<ma;++it)
+				{
+					for(i=0;i<4 && it<ma;)
+					{
+						boost::uint32_t idx_i=idx[i]=m_rng_gen();
+						std::size_t j=0;
+						for(;j<i;++j)
+							if(idx_i==idx[j])
+								break;
+						if(j<i)
+							continue;
+						
+						ms1[i]=m_ops[m_oim[idx_i].first];
+						ms2[i]=m_ips[m_oim[idx_i].second];
+						i++;
+					}
+					if(i==4 && (!check_sub_set(ms1,i) || !check_sub_set(ms2,i)))
+						continue;
+					break;
+				}
+				return i==4 && it<ma;
+			}
+
+			bool check_sub_set(std::vector<point2_t> const &ms, std::size_t n)
+			{
+				std::size_t i=0;
+				for(;i<=(n-1);++i)
+				{
+					std::size_t j=0;
+					for(;j<i;j++)
+					{
+						value_t dx1=ms[j].x-ms[i].x;
+						value_t dy1=ms[j].y-ms[i].y;
+						std::size_t k=0;
+						for(;k<j;k++)
+						{
+							value_t dx2=ms[k].x-ms[i].x;
+							value_t dy2=ms[k].y-ms[i].y;
+							if(fabs(dx2*dy1-dy2*dx1)<=std::numeric_limits<value_t>::epsilon()*(fabs(dx1)+fabs(dy1)+fabs(dx2)+fabs(dy2)))
+								break;
+						}
+						if(k<j)
+							break;
+					}
+					if(j<i)
+						break;
+				}
+				return i>=n-1;
+			}
+
 			bool run_kernel(std::vector<point2_t> const &ms1, std::vector<point2_t> const &ms2, matrix_t &models)
 			{
 				BOOST_ASSERT(ms1.size()==ms2.size());
@@ -195,11 +265,25 @@ namespace baldzarika { namespace ucv {
 				return true;
 			}
 
+			void compute_reproj_error(matrix_t &models, std::vector<value_t> &err)
+			{
+				typename matrix_t::array_type const &H=models.data();
+				for(std::size_t i=0;i<m_oim.size();++i)
+				{
+					value_t ww=detail::constants::one<value_t>()/(H[6]*m_ops[m_oim[i].first].x+H[7]*m_ops[m_oim[i].first].y+detail::constants::one<value_t>());
+					value_t dx=(H[0]*m_ops[m_oim[i].first].x+H[1]*m_ops[m_oim[i].first].y+H[2])*ww-m_ips[m_oim[i].second].x;
+					value_t dy=(H[3]*m_ops[m_oim[i].first].x+H[4]*m_ops[m_oim[i].first].y+H[5])*ww-m_ips[m_oim[i].second].y;
+					err[i]=(dx*dx + dy*dy);
+				}
+			}
+
 		private:
 			feature_points_t const	&m_ops;
 			feature_points_t const	&m_ips;
 			matches_t const			&m_oim;
-			matrix_t				&m_hm;
+			boost::mt19937			m_rng;
+			uint32_rng_t			m_rng_gen;
+			
 		};
 
 		template < typename T, typename DT >
@@ -211,16 +295,16 @@ namespace baldzarika { namespace ucv {
 
 	}
 
-	template < typename T, typename DT >
+	template < typename T, typename DT, typename AS>
 	bool find_homography(
 		std::vector< feature_point<T,DT> > const &ops,
 		std::vector< feature_point<T,DT> > const &ips,
 		std::vector< std::pair<std::size_t, std::size_t> > const &oim,
-		nublas::matrix< typename feature_point<T,DT>::value_type > &hm
+		nublas::matrix< typename feature_point<T,DT>::value_type, nublas::row_major, AS > &hm
 	)
 	{
-		detail::homography_estimator<T,DT> hest(ops, ips, oim, hm);
-		return hest();
+		detail::homography_estimator<T,DT> hest(ops, ips, oim);
+		return hest(hm);
 	}
 
 } //namespace ucv
