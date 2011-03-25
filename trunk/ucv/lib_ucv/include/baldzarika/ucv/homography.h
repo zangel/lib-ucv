9ce5dc9a09609400684493b5b1fa8a31303d2c8c
baldzar@gmail.com <baldzar@gmail.com@aa6cf039-9e87-f74f-3bf4-c4349377eac6>
2011-03-25 02:01:43 +0000

    switched to ransac homography estimation

--- trunk/ucv/lib_ucv/include/baldzarika/ucv/homography.h
+++ trunk/ucv/lib_ucv/include/baldzarika/ucv/homography.h
@@ -1,331 +1,82 @@
 #ifndef BALDZARIKA_UCV_HOMOGRAPHY_H
 #define BALDZARIKA_UCV_HOMOGRAPHY_H
 
-#include <baldzarika/ucv/lapack/jacobi.h>
+#include <baldzarika/ucv/point2.h>
+#include <baldzarika/ucv/feature_point.h>
 
 namespace baldzarika { namespace ucv {
 
-	namespace detail {
+	class homography
+	{
+	public:
+		typedef point2<float> point2_t;
+		typedef ublas::matrix<float, ublas::row_major> matrix_t;
+		typedef ublas::vector<float> vector_t;
+		typedef boost::uniform_int<boost::uint32_t> uint32_dist_t;
+		typedef boost::variate_generator<boost::mt19937&, uint32_dist_t> uint32_rng_t;
+		
+		homography(std::vector<point2_t> const &obj_pts, std::vector<point2_t> const &img_pts);
+		~homography();
 
-		template < typename T, typename DT>
-		class homography_estimator
-		{
-		public:
-			static T const CONFIDENCE;
-			static T const OUTLIER_RATIO;
-			static std::size_t const MAX_ITERS;
-
-			typedef feature_point<T,DT> feature_point_t;
-			typedef typename feature_point_t::point2_t point2_t;
-
-			typedef typename feature_point_t::value_type value_t;
-			typedef std::vector<feature_point_t> feature_points_t;
-			typedef std::pair<std::size_t, std::size_t> match_t;
-			typedef std::vector<match_t> matches_t;
-			typedef nublas::matrix<value_t, nublas::row_major> matrix_t;
-
-			typedef boost::uniform_int<boost::uint32_t> uint32_dist_t;
-			typedef boost::variate_generator<boost::mt19937&, uint32_dist_t> uint32_rng_t;
-						
-			homography_estimator(feature_points_t const &ops, feature_points_t const &ips, matches_t const &oim)
-				: m_ops(ops)
-				, m_ips(ips)
-				, m_oim(oim)
-				, m_rng()
-				, m_rng_gen(m_rng, uint32_dist_t(0,m_oim.size()-1))
-			{
-			}
-
-			template < typename AS >
-			bool operator()(nublas::matrix< value_t, nublas::row_major, AS> &hm, value_t const &ic)
-			{
-				if(m_oim.size()<4)
-					return false;
-
-				value_t min_median=std::numeric_limits<value_t>::max();
-				matrix_t models(3,3);
-				
-				std::vector<value_t> err(m_oim.size());
-				std::vector<point2_t> ms1(4), ms2(4);
+	protected:
+		static bool		normalize_points(std::vector<point2_t> const &pts, point2_t &tra, point2_t &sca, matrix_t &m33, bool inv=false);
+		bool			get_sub_set(std::vector<point2_t> &obj_pts, std::vector<point2_t> &img_pts, std::size_t ma);
+		bool			check_sub_set(std::vector<point2_t> const &pts, std::size_t n);
+		bool			run_kernel(std::vector<point2_t> const &obj_pts, std::vector<point2_t> const &img_pts, matrix_t &h);
+		std::size_t		inliers_count(matrix_t &ms, float t);
+		void			compute_reproj_error(matrix_t &ms, std::vector<float> &err);
 
 
-				std::size_t n_iters=m_oim.size()==4?
-					1:
-					round<std::size_t>(
-						log(detail::constants::one<T>()-CONFIDENCE)/
-						log(detail::constants::one<T>()-pow(detail::constants::one<T>()-OUTLIER_RATIO, value_t(4)))
-					);
+	private:
+		static bool		jacobi(matrix_t const &S0, vector_t &e, matrix_t &E, bool ce=true, float eps=std::numeric_limits<float>::epsilon());
+	
+	
+	protected:
+		std::vector<point2_t> const &m_obj_pts;
+		std::vector<point2_t> const &m_img_pts;
 
-				if(m_oim.size()==4)
-				{
-					
-					for(std::size_t m=0;m<m_oim.size();++m)
-					{
-						ms1[m]=m_ops[m_oim[m].first];
-						ms2[m]=m_ips[m_oim[m].second];
-					}
-				}
-				
-				n_iters=std::min<std::size_t>( n_iters, MAX_ITERS);
+		boost::mt19937				m_rng;
+		uint32_rng_t				m_rng_gen;
+	};
 
-				for(std::size_t it=0;it<n_iters;++it)
-				{
-					if(m_oim.size()>4)
-					{
-						if(!get_sub_set(ms1,ms2, 300))
-						{
-							if(!it) return false;
-							break;
-						}
-					}
-					if(!run_kernel(ms1, ms2, models))
-						continue;
-					compute_reproj_error(models, err);
-					std::sort(err.begin(), err.end(), std::less<value_t>());
-					value_t median=m_oim.size()%2!=0?
-						err[m_oim.size()/2]:
-					(err[m_oim.size()/2-1]+err[m_oim.size()/2])*detail::constants::half<value_t>();
-					if(median<min_median)
-					{
-						min_median=median;
-						hm=models;
-					}
-				}
+	class ransac_homography
+		: public homography
+	{
+	public:
+		ransac_homography(std::vector<point2_t> const &obj_pts, std::vector<point2_t> const &img_pts);
+		~ransac_homography();
 
-				if(min_median<std::numeric_limits<value_t>::max())
-				{
-					static value_t const _sigma_c=2.5*1.4826;
-					static value_t const _sigma_max=0.001;
-					
-					value_t sigma=_sigma_c*
-						(detail::constants::one<value_t>()+
-							value_t(5)/
-							std::max(detail::constants::one<value_t>(), value_t(m_oim.size()-4))
-						)*
-						sqrt(min_median);
-					sigma=std::max(sigma, _sigma_max);
+		bool			operator()(homography::matrix_t &h, float t, float c=0.99f, std::size_t mi=2000);
 
-					return inliers_count(hm, sigma)>=ucv::round<boost::uint32_t>(value_t(m_oim.size())*ic);
-				}
-				return false;
-			}
+	protected:
+		std::size_t		update_num_iters(float p, float ep, std::size_t mi, std::size_t nmp=4);
+	};
 
-		protected:
-			bool get_sub_set(std::vector<point2_t> &ms1, std::vector<point2_t> &ms2, std::size_t ma)
-			{
-				boost::uint32_t idx[4];
-				std::size_t it=0, i=0;
-				for(; it<ma;++it)
-				{
-					for(i=0;i<4 && it<ma;)
-					{
-						boost::uint32_t idx_i=idx[i]=m_rng_gen();
-						std::size_t j=0;
-						for(;j<i;++j)
-							if(idx_i==idx[j])
-								break;
-						if(j<i)
-							continue;
-						
-						ms1[i]=m_ops[m_oim[idx_i].first];
-						ms2[i]=m_ips[m_oim[idx_i].second];
-						i++;
-					}
-					if(i==4 && (!check_sub_set(ms1,i) || !check_sub_set(ms2,i)))
-						continue;
-					break;
-				}
-				return i==4 && it<ma;
-			}
-
-			bool check_sub_set(std::vector<point2_t> const &ms, std::size_t n)
-			{
-				std::size_t i=0;
-				for(;i<=(n-1);++i)
-				{
-					std::size_t j=0;
-					for(;j<i;j++)
-					{
-						value_t dx1=ms[j].x-ms[i].x;
-						value_t dy1=ms[j].y-ms[i].y;
-						std::size_t k=0;
-						for(;k<j;k++)
-						{
-							value_t dx2=ms[k].x-ms[i].x;
-							value_t dy2=ms[k].y-ms[i].y;
-							if(fabs(dx2*dy1-dy2*dx1)<=std::numeric_limits<value_t>::epsilon()*(fabs(dx1)+fabs(dy1)+fabs(dx2)+fabs(dy2)))
-								break;
-						}
-						if(k<j)
-							break;
-					}
-					if(j<i)
-						break;
-				}
-				return i>=n-1;
-			}
-
-			bool run_kernel(std::vector<point2_t> const &ms1, std::vector<point2_t> const &ms2, matrix_t &models)
-			{
-				BOOST_ASSERT(ms1.size()==ms2.size());
-
-				value_t count_(ms1.size());
-				value_t LtL[9][9], W[9][9], V[9][9];
-				typename feature_point_t::point2_t cM(0,0), cm(0,0), sM(0,0), sm(0,0);
-				
-				for(std::size_t i=0;i<ms1.size();++i)
-				{
-					cm.x+=ms2[i].x; cm.y+=ms2[i].y;
-					cM.x+=ms1[i].x; cM.y+=ms1[i].y;
-				}
-
-				cm.x/=count_; cm.y/=count_;
-				cM.x/=count_; cM.y/=count_;
-				
-				for(std::size_t i=0;i<ms1.size();++i)
-				{
-					sm.x+=fabs(ms2[i].x-cm.x);
-					sm.y+=fabs(ms2[i].y-cm.y);
-					sM.x+=fabs(ms1[i].x-cM.x);
-					sM.y+=fabs(ms1[i].y-cM.y);
-				}
-
-				if(	fabs(sm.x)<std::numeric_limits<value_t>::epsilon() || fabs(sm.y)<std::numeric_limits<value_t>::epsilon() ||
-					fabs(sM.x)<std::numeric_limits<value_t>::epsilon() || fabs(sM.y)<std::numeric_limits<value_t>::epsilon() )
-					return false;
-				sm.x=count_/sm.x; sm.y=count_/sm.y;
-				sM.x=count_/sM.x; sM.y=count_/sM.y;
-				
-				value_t invHnorm[9]=
-				{
-					detail::constants::one<value_t>()/sm.x,	detail::constants::zero<value_t>(),		cm.x,
-					detail::constants::zero<value_t>(),		detail::constants::one<value_t>()/sm.y,	cm.y,
-					detail::constants::zero<value_t>(),		detail::constants::zero<value_t>(),		detail::constants::one<value_t>()
-				};
-				
-				value_t Hnorm2[9]=
-				{
-					sM.x,								detail::constants::zero<value_t>(),		-cM.x*sM.x,
-					detail::constants::zero<value_t>(),	sM.y,									-cM.y*sM.y,
-					detail::constants::zero<value_t>(), detail::constants::zero<value_t>(),		detail::constants::one<value_t>()
-				};
-
-				for(std::size_t i=0;i<9;++i)
-					for(std::size_t j=0;j<9;++j)
-						LtL[i][j]=detail::constants::zero<value_t>();
-				
-				for(std::size_t i=0;i<ms1.size();++i)
-				{
-					value_t x=(ms2[i].x-cm.x)*sm.x, y=(ms2[i].y-cm.y)*sm.y;
-					value_t X=(ms1[i].x-cM.x)*sM.x, Y=(ms1[i].y-cM.y)*sM.y;
-					value_t Lx[9]=
-					{
-						X,									Y,									detail::constants::one<value_t>(),
-						detail::constants::zero<value_t>(), detail::constants::zero<value_t>(),	detail::constants::zero<value_t>(),
-						-x*X,								-x*Y,								-x
-					};
-
-					value_t Ly[9]=
-					{
-						detail::constants::zero<value_t>(),	detail::constants::zero<value_t>(), detail::constants::zero<value_t>(),
-						X,									Y,									detail::constants::one<value_t>(),
-						-y*X,								-y*Y,								-y
-					};
-
-					for(std::size_t j=0;j<9;++j)
-					{
-						for(std::size_t k=j;k<9;++k)
-						{
-							LtL[j][k]+=Lx[j]*Lx[k]+Ly[j]*Ly[k];
-						}
-					}
-				}
-
-				for(std::size_t i=0;i<9;++i)
-				{
-					for(std::size_t j=0;j<i;++j)
-					{
-						LtL[i][j]=LtL[j][i];
-					}
-				}
-
-				{
-					typedef nublas::matrix<value_t, nublas::row_major, nublas::shallow_array_adaptor<value_t> > shalow_adapted_mat_t;
-
-					shalow_adapted_mat_t LtL_(9, 9, nublas::shallow_array_adaptor<value_t>(9*9, &LtL[0][0]) );
-					shalow_adapted_mat_t W_(9, 9, nublas::shallow_array_adaptor<value_t>(9*9, &W[0][0]) );
-					shalow_adapted_mat_t V_(9, 9, nublas::shallow_array_adaptor<value_t>(9*9, &V[0][0]) );
-
-
-					lapack::jacobi(LtL_, W_, V_);
-
-					shalow_adapted_mat_t _H0(3,3, nublas::shallow_array_adaptor<value_t>(3*3, V[8]));
-					shalow_adapted_mat_t _Htemp(3,3, nublas::shallow_array_adaptor<value_t>(3*3, V[7]));
-					shalow_adapted_mat_t _invHnorm(3,3, nublas::shallow_array_adaptor<value_t>(3*3, invHnorm));
-					shalow_adapted_mat_t _Hnorm2(3,3, nublas::shallow_array_adaptor<value_t>(3*3, Hnorm2));
-				
-					_Htemp=nublas::prod(_invHnorm,_H0);
-					_H0=nublas::prod(_Htemp, _Hnorm2);
-					//float d_H0=static_cast<float>(_H0(2,2));
-					models=_H0*(detail::constants::one<value_t>()/_H0(2,2));
-				}
-				return true;
-			}
-
-			void compute_reproj_error(matrix_t &models, std::vector<value_t> &err)
-			{
-				typename matrix_t::array_type const &H=models.data();
-				for(std::size_t i=0;i<m_oim.size();++i)
-				{
-					value_t ww=detail::constants::one<value_t>()/(H[6]*m_ops[m_oim[i].first].x+H[7]*m_ops[m_oim[i].first].y+detail::constants::one<value_t>());
-					value_t dx=(H[0]*m_ops[m_oim[i].first].x+H[1]*m_ops[m_oim[i].first].y+H[2])*ww-m_ips[m_oim[i].second].x;
-					value_t dy=(H[3]*m_ops[m_oim[i].first].x+H[4]*m_ops[m_oim[i].first].y+H[5])*ww-m_ips[m_oim[i].second].y;
-					err[i]=(dx*dx + dy*dy);
-				}
-			}
-
-			boost::uint32_t inliers_count(matrix_t &models, value_t const &t)
-			{
-				value_t treshold=t*t;
-
-				std::vector<value_t> err(m_oim.size());
-				compute_reproj_error(models, err);
-
-				boost::uint32_t n_inliers=0;
-				for(std::size_t i=0;i<m_oim.size();++i)
-					n_inliers+=err[i]<treshold?1:0;
-				return n_inliers;
-			}
-
-		private:
-			feature_points_t const	&m_ops;
-			feature_points_t const	&m_ips;
-			matches_t const			&m_oim;
-			boost::mt19937			m_rng;
-			uint32_rng_t			m_rng_gen;
-			
-		};
-
-		template < typename T, typename DT >
-		T const homography_estimator<T,DT>::CONFIDENCE=0.995;
-		template < typename T, typename DT >
-		T const homography_estimator<T,DT>::OUTLIER_RATIO=0.45;
-		template < typename T, typename DT >
-		std::size_t const homography_estimator<T,DT>::MAX_ITERS=2000;
-
-	}
-
-	template < typename T, typename DT, typename AS>
-	bool find_homography(
+	template <typename T, typename DT>
+	bool find_homography_ransac(
 		std::vector< feature_point<T,DT> > const &ops,
 		std::vector< feature_point<T,DT> > const &ips,
 		std::vector< std::pair<std::size_t, std::size_t> > const &oim,
-		nublas::matrix< typename feature_point<T,DT>::value_type, nublas::row_major, AS > &hm,
-		T const &ic=0.5
+		homography::matrix_t &hm,
+		float rt=3.0f
 	)
 	{
-		detail::homography_estimator<T,DT> hest(ops, ips, oim);
-		return hest(hm, ic);
+		BOOST_ASSERT(hm.size1()==3 && hm.size2()==3);
+		
+		std::vector<homography::point2_t> obj_pts(oim.size()), img_pts(oim.size());
+		for(std::size_t p=0;p<oim.size();++p)
+		{
+			obj_pts[p]=homography::point2_t(
+				static_cast<homography::point2_t::value_type>(ops[oim[p].first].x),
+				static_cast<homography::point2_t::value_type>(ops[oim[p].first].y)
+			);
+
+			img_pts[p]=homography::point2_t(
+				static_cast<homography::point2_t::value_type>(ips[oim[p].second].x),
+				static_cast<homography::point2_t::value_type>(ips[oim[p].second].y)
+			);
+		}
+		return ransac_homography(obj_pts,img_pts)(hm,rt);
 	}
 
 } //namespace ucv
---
+++ trunk/ucv/lib_ucv/src/baldzarika/ucv/homography.cpp
@@ -0,0 +1,453 @@
+#include <baldzarika/ucv/config.h>
+#include <baldzarika/ucv/homography.h>
+
+namespace baldzarika { namespace ucv {
+
+	namespace {
+
+		static inline float hypot(float a, float b)
+		{
+			a=std::abs(a);
+			b=std::abs(b);
+			if(a>b)
+			{
+				float f=b/a;
+				return a*std::sqrt(1.0f+f*f);
+			}
+			if(b<std::numeric_limits<float>::epsilon())
+				return 0.0f;
+			float f=a/b;
+			return b*std::sqrt(1.0f+f*f);
+		}
+	
+	} //namespace anonymous
+
+
+
+	homography::homography(std::vector<point2_t> const &obj_pts, std::vector<point2_t> const &img_pts)
+		: m_obj_pts(obj_pts)
+		, m_img_pts(img_pts)
+		, m_rng()
+		, m_rng_gen(m_rng, uint32_dist_t(0, m_obj_pts.size()-1))
+	{
+		BOOST_ASSERT(m_obj_pts.size()==m_img_pts.size());
+	}
+
+	homography::~homography()
+	{
+	}
+
+
+	bool homography::get_sub_set(std::vector<point2_t> &obj_pts, std::vector<point2_t> &img_pts, std::size_t ma)
+	{
+		BOOST_ASSERT(obj_pts.size()==img_pts.size());
+		
+		boost::uint32_t idx[4];
+		std::size_t it=0, i=0;
+		for(; it<ma;++it)
+		{
+			for(i=0;i<4 && it<ma;)
+			{
+				boost::uint32_t idx_i=idx[i]=m_rng_gen();
+				std::size_t j=0;
+				for(;j<i;++j)
+					if(idx_i==idx[j])
+						break;
+				if(j<i)
+					continue;
+
+				obj_pts[i]=m_obj_pts[idx_i];
+				img_pts[i]=m_img_pts[idx_i];
+				i++;
+			}
+			if(i==4 && (!check_sub_set(obj_pts,i) || !check_sub_set(img_pts,i)))
+				continue;
+			break;
+		}
+		return i==4 && it<ma;
+	}
+
+	bool homography::check_sub_set(std::vector<point2_t> const &pts, std::size_t n)
+	{
+		std::size_t i=0;
+		for(;i<=(n-1);++i)
+		{
+			std::size_t j=0;
+			for(;j<i;j++)
+			{
+				float dx1=pts[j].x-pts[i].x;
+				float dy1=pts[j].y-pts[i].y;
+				std::size_t k=0;
+				for(;k<j;k++)
+				{
+					float dx2=pts[k].x-pts[i].x;
+					float dy2=pts[k].y-pts[i].y;
+					if(std::abs(dx2*dy1-dy2*dx1)<=std::numeric_limits<float>::epsilon()*(std::abs(dx1)+std::abs(dy1)+std::abs(dx2)+std::abs(dy2)))
+						break;
+				}
+				if(k<j)
+					break;
+			}
+			if(j<i)
+				break;
+		}
+		return i>=n-1;
+	}
+
+	void homography::compute_reproj_error(matrix_t &ms, std::vector<float> &err)
+	{
+		BOOST_ASSERT(m_obj_pts.size()==m_img_pts.size());
+		matrix_t::array_type const &H=ms.data();
+		for(std::size_t i=0;i<m_obj_pts.size();++i)
+		{
+			float ww=1.0f/(H[6]*m_obj_pts[i].x+H[7]*m_obj_pts[i].y+1.0f);
+			float dx=(H[0]*m_obj_pts[i].x+H[1]*m_obj_pts[i].y+H[2])*ww-m_img_pts[i].x;
+			float dy=(H[3]*m_obj_pts[i].x+H[4]*m_obj_pts[i].y+H[5])*ww-m_img_pts[i].y;
+			err[i]=(dx*dx + dy*dy);
+		}
+	}
+
+	std::size_t homography::inliers_count(matrix_t &ms, float t)
+	{
+		BOOST_ASSERT(m_obj_pts.size()==m_img_pts.size());
+		
+		float treshold=t*t;
+
+		std::vector<float> err(m_obj_pts.size());
+		compute_reproj_error(ms, err);
+
+		std::size_t n_inliers=0;
+		for(std::size_t i=0;i<m_obj_pts.size();++i)
+			n_inliers+=err[i]<treshold?1:0;
+		return n_inliers;
+	}
+
+	bool homography::run_kernel(std::vector<point2_t> const &obj_pts, std::vector<point2_t> const &img_pts, matrix_t &h)
+	{
+		BOOST_ASSERT(obj_pts.size()==img_pts.size());
+		BOOST_ASSERT(obj_pts.size()>=4);
+
+		point2_t obj_translate, obj_scale;
+		matrix_t obj_correct(3,3);
+		normalize_points(obj_pts, obj_translate, obj_scale, obj_correct, true);
+
+		point2_t img_translate, img_scale;
+		matrix_t img_correct(3,3);
+		normalize_points(img_pts, img_translate, img_scale, img_correct, false);
+
+		matrix_t LtL=ublas::zero_matrix<float>(9,9);
+
+		for(std::size_t i=0;i<obj_pts.size();++i)
+		{
+			float obj_x=(obj_pts[i].x-obj_translate.x)/obj_scale.x;
+			float obj_y=(obj_pts[i].y-obj_translate.y)/obj_scale.y;
+
+			float img_x=(img_pts[i].x-img_translate.x)/img_scale.x;
+			float img_y=(img_pts[i].y-img_translate.y)/img_scale.y;
+			float Lx[9]=
+			{
+				obj_x,			obj_y,			1.0f,
+				0.0f,			0.0f,			0.0f,
+				-img_x*obj_x,	-img_x*obj_y,	-img_x
+			};
+
+			float Ly[9]=
+			{
+				0.0f,			0.0f,			0.0f,
+				obj_x,			obj_y,			1.0f,
+				-img_y*obj_x,	-img_y*obj_y,	-img_y
+			};
+
+			for(std::size_t j=0;j<9;++j)
+				for(std::size_t k=j;k<9;++k)
+					LtL(j,k)+=Lx[j]*Lx[k]+Ly[j]*Ly[k];
+
+		}
+		
+		for(std::size_t i=0;i<9;++i)
+		{
+			for(std::size_t j=0;j<i;++j)
+			{
+				LtL(i,j)=LtL(j,i);
+			}
+		}
+
+		matrix_t V(9,9);
+		vector_t W(9);
+		jacobi(LtL, W, V);
+
+		matrix_t H(3,3);
+		H(0,0)=V(8,0);H(0,1)=V(8,1);H(0,2)=V(8,2);
+		H(1,0)=V(8,3);H(1,1)=V(8,4);H(1,2)=V(8,5);
+		H(2,0)=V(8,6);H(2,1)=V(8,7);H(2,2)=V(8,8);
+
+		// reverse normalization
+		matrix_t Htemp(ublas::prod(img_correct,H));
+		ublas::noalias(H)=ublas::prod(Htemp,obj_correct);
+		h=H*(1.0f/H(2,2));
+		return true;
+
+	}
+
+	bool homography::normalize_points(std::vector<homography::point2_t> const &pts, homography::point2_t &tra, homography::point2_t &sca, homography::matrix_t &m33, bool inv)
+	{
+		float const inv_size=1.0f/static_cast<float>(pts.size());
+
+		point2_t translate(0.0f,0.0f), scale(0.0f,0.0f);
+
+		for(std::size_t p=0;p<pts.size();++p)
+		{
+			translate.x+=pts[p].x;
+			translate.y+=pts[p].y;
+
+			scale.x+=pts[p].x*pts[p].x;
+			scale.y+=pts[p].y*pts[p].y;
+		}
+
+		translate.x*=inv_size;
+		translate.y*=inv_size;
+		scale.x*=inv_size;
+		scale.y*=inv_size;
+
+		scale.x=std::sqrt(scale.x-translate.x*translate.x);
+		scale.y=std::sqrt(scale.y-translate.y*translate.y);
+
+		if(std::abs(scale.x)<std::numeric_limits<float>::epsilon() || std::abs(scale.y)<std::numeric_limits<float>::epsilon())
+			return false;
+
+		m33=ublas::zero_matrix<float>(3,3);
+		m33(2,2)=1.0f;
+
+		m33(0,0)=inv?1.0f/scale.x:scale.x;
+		m33(1,1)=inv?1.0f/scale.y:scale.y;
+
+		m33(0,2)=inv?-1.0f/scale.x*translate.x:translate.x;
+		m33(1,2)=inv?-1.0f/scale.y*translate.y:translate.y;
+		tra=translate;
+		sca=scale;
+		return true;
+	}
+
+	bool homography::jacobi(homography::matrix_t const &S0, homography::vector_t &e, homography::matrix_t &E, bool ce, float eps)
+	{
+		BOOST_ASSERT(S0.size1()==S0.size2() && S0.size1()==9);
+		BOOST_ASSERT(e.size()==9);
+		BOOST_ASSERT(E.size1()==E.size2() && E.size1()==9);
+
+		boost::int32_t max_iters=9*9*30;;
+		boost::int32_t i, j, k, m, it;
+
+		vector_t maxSR(9), maxSC(9);
+		boost::int32_t indR[9], indC[9];
+
+		matrix_t S=S0;
+
+		if(ce) E=ublas::identity_matrix<float>(9,9);
+
+		float mv;
+
+		for(k=0;k<9;++k)
+		{
+			e(k)=S(k,k);
+			if(k<8)
+			{
+				for(m=k+1,mv=std::abs(S(k,m)),i=k+2;i<9;++i)
+				{
+					float v=std::abs(S(k,i));
+					if(mv<v)
+						mv=v,m=i;
+				}
+				maxSR(k)=mv;
+				indR[k]=m;
+			}
+			if(k>0)
+			{
+				for(m=0,mv=std::abs(S(0,k)),i=1;i<k;++i)
+				{
+					float v=std::abs(S(i,k));
+					if(mv<v)
+						mv=v,m=i;
+				}
+				maxSC(k)=mv;
+				indC[k]=m;
+			}
+		}
+
+		for(it=0;it<max_iters;++it)
+		{
+			for(k=0,mv=maxSR(0),i=1;i<8;++i)
+			{
+				float v=maxSR(i);
+				if(mv<v)
+					mv=v,k=i;
+			}
+
+			boost::int32_t l=indR[k];
+			for(i=1;i<9;++i)
+			{
+				float v=maxSC(i);
+				if(mv<v)
+					mv=v,k=indC[i],l=i;
+			}
+
+			float p=S(k,l);
+			if(std::abs(p)<=eps)
+				break;
+			float y=(e(l)-e(k))*0.5f;
+			float t=std::abs(y)+hypot(p,y);
+			float s=hypot(p,t);
+			float c=t/s;
+			s=p/s; t=(p/t)*p;
+			if(y<0.0f)
+				s=-s,t=-t;
+
+			S(k,l)=0.0f;
+			e(k)-=t;
+			e(l)+=t;
+
+			float a0, b0;
+
+#undef rotate
+#define rotate(v0, v1) a0 = v0, b0 = v1, v0 = a0*c - b0*s, v1 = a0*s + b0*c
+			for(i=0;i<k;++i)
+				rotate(S(i,k), S(i,l));
+			for(i=k+1;i<l;++i)
+				rotate(S(k,i), S(i,l));
+			for(i=l+1;i<9;++i)
+				rotate(S(k,i), S(l,i));
+			if(ce)
+			{
+				for(i=0;i<9;++i)
+				{
+					rotate(E(k,i), E(l,i));
+				}
+			}
+#undef rotate
+			for(j=0;j<2;++j)
+			{
+				boost::int32_t idx=j==0?k:l;
+				if(idx<8)
+				{
+					for(m=idx+1,mv=std::abs(S(idx,m)),i=idx+2;i<9;++i)
+					{
+						float v=std::abs(S(idx,i));
+						if(mv<v)
+							mv=v,m=i;
+					}
+					maxSR(idx)=mv;
+					indR[idx]=m;
+				}
+				if(idx>0)
+				{
+					for(m=0,mv=std::abs(S(0,idx)),i=1;i<idx;++i)
+					{
+						float v=std::abs(S(i,idx));
+						if(mv<v)
+							mv=v,m=i;
+					}
+					maxSC(idx)=mv;
+					indC[idx]=m;
+				}
+			}
+		}
+		for(k=0;k<8;++k)
+		{
+			m=k;
+			for(i=k+1;i<9;++i)
+			{
+				if(e(m)<e(i))
+					m=i;
+			}
+			if(k!=m)
+			{
+				std::swap(e(m), e(k));
+				if(ce)
+				{
+					for(i=0;i<9;++i)
+						std::swap(E(m,i),E(k,i));
+				}
+			}
+		}
+		return true;
+	}
+
+
+	ransac_homography::ransac_homography(std::vector<point2_t> const &obj_pts, std::vector<point2_t> const &img_pts)
+		: homography(obj_pts,img_pts)
+	{
+
+	}
+
+	ransac_homography::~ransac_homography()
+	{
+
+	}
+
+	std::size_t ransac_homography::update_num_iters(float p, float ep, std::size_t mi, std::size_t nmp)
+	{
+		p=std::max(p,0.0f);
+		p=std::min(p,1.0f);
+		ep=std::max(ep,0.0f);
+		ep=std::min(ep,1.0f);
+		
+		float num=std::max(1.0f-p, std::numeric_limits<float>::min());
+		float denom=1.0f-std::pow(1.0f-ep, static_cast<float>(nmp));
+		if(denom<std::numeric_limits<float>::min())
+			return 0;
+		num=std::log(num);
+		denom=std::log(denom);
+		return denom>=0.0f || -num>=mi*(-denom)?
+			mi:
+			static_cast<std::size_t>(boost::math::round(num/denom));
+	}
+
+	bool ransac_homography::operator()(homography::matrix_t &h, float t, float c, std::size_t mi)
+	{
+		if(m_obj_pts.size()<4 || m_obj_pts.size()!=m_img_pts.size())
+			return false;
+		
+		std::vector<point2_t> obj_pts(4), img_pts(4);
+		std::size_t n_iters=m_img_pts.size()==4?1:mi;
+
+		if(m_obj_pts.size()==4)
+		{
+			n_iters=1;
+			for(std::size_t m=0;m<m_obj_pts.size();++m)
+			{
+				obj_pts[m]=m_obj_pts[m];
+				img_pts[m]=m_img_pts[m];
+			}
+		}
+
+		matrix_t ms(3,3);
+
+		std::size_t max_good_count=0;
+		for(std::size_t it=0;it<n_iters;++it)
+		{
+			if(m_obj_pts.size()>4)
+			{
+				if(!get_sub_set(obj_pts,img_pts, 300))
+				{
+					if(!it) return false;
+					break;
+				}
+			}
+			if(!run_kernel(obj_pts, img_pts, ms))
+				continue;
+			std::size_t good_count=inliers_count(ms,t);
+			if(good_count>std::max(max_good_count,std::size_t(3)))
+			{
+				max_good_count=good_count;
+				h=ms;
+				n_iters=update_num_iters(
+					c,
+					static_cast<float>(m_obj_pts.size()-good_count)/static_cast<float>(m_obj_pts.size()),
+					n_iters
+				);
+			}
+		}
+		return max_good_count>0;
+	}
+
+} //namespace ucv
+} //namespace baldzarika
