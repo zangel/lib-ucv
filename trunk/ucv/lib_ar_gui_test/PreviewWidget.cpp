6b3fc23453e14397e4f13d15efe50322615464df
baldzar@gmail.com <baldzar@gmail.com@aa6cf039-9e87-f74f-3bf4-c4349377eac6>
2011-04-10 23:03:52 +0000

    * added lib_ar_gui_test qt application

---
+++ trunk/ucv/lib_ar_gui_test/PreviewWidget.cpp
@@ -0,0 +1,12 @@
+#include "Prec.h"
+#include "PreviewWidget.h"
+
+PreviewWidget::PreviewWidget(QWidget *parent/* =0 */)
+	: QGLWidget(parent)
+{
+}
+
+PreviewWidget::~PreviewWidget()
+{
+
+}
---
+++ trunk/ucv/lib_ar_gui_test/main.cpp
@@ -0,0 +1,10 @@
+#include "Prec.h"
+#include "ArTestDlg.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	ArTestDlg w;
+	w.show();
+	return a.exec();
+}
