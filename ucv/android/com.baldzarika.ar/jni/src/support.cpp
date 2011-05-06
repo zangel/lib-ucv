cbc1547548b3748254a668233dc01f54322c5c5a
baldzar <baldzar@gmail.com>
2011-05-06 14:51:49 +0000

    * canny edge detection stuff

--- ucv/android/com.baldzarika.ar/jni/src/support.cpp
+++ ucv/android/com.baldzarika.ar/jni/src/support.cpp
@@ -16,6 +16,8 @@ namespace {
 
 	using namespace j2cpp;
 	using namespace j2cpp::java::lang;
+	using namespace com::baldzarika::ar;
+
 
 	class custom_environment
 		: public j2cpp::raw_environment
@@ -32,17 +34,34 @@ namespace {
 
 		bool init()
 		{
+#if 0
 			if(local_ref<Thread> currThread=Thread::currentThread())
 			{
 				if(local_ref<ClassLoader> cler=currThread->getContextClassLoader())
 				{
-					currThread->setContextClassLoader(cler);
+					//currThread->setContextClassLoader(cler);
 					if(local_ref<Class> dummyClass=cler->loadClass(local_ref<String>(jenv()->NewStringUTF("java/lang/String"))))
 					{
 						m_class_loader=cler;
 					}
 				}
 			}
+#else
+			if(local_ref<Size2> dummySize2=local_ref<Size2>(Size2()))
+			{
+				if(local_ref< Object > dummySize2Obj=static_cast< local_ref<Object> >(dummySize2))
+				{
+					if(local_ref< Class > dummySize2Class=dummySize2Obj->getClass())
+					{
+						if(local_ref<ClassLoader> cler=dummySize2Class->getClassLoader())
+						{
+							if(local_ref<Class> dummyClass=cler->loadClass(local_ref<String>(jenv()->NewStringUTF("java/lang/String"))))
+								m_class_loader=cler;
+						}
+					}
+				}
+			}
+#endif
 			return static_cast<bool>(m_class_loader);
 		}
 
@@ -52,11 +71,11 @@ namespace {
 			{
 				if(m_class_loader)
 				{
-					if(local_ref<Thread> currThread=Thread::currentThread())
-					{
+					//if(local_ref<Thread> currThread=Thread::currentThread())
+					//{
 						//currThread->setPriority(10);
 						return true;
-					}
+					//}
 				}
 			}
 			return false;
--- ucv/lib_ucv/include/baldzarika/ucv/config.h
+++ ucv/lib_ucv/include/baldzarika/ucv/config.h
@@ -43,6 +43,7 @@
 
 
 #include <vector>
+#include <stack>
 #include <list>
 
 #include <kdtree++/kdtree.hpp>
