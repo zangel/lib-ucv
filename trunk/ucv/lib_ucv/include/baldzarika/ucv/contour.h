57a0bbaadeab4fe975a112e12b68e2373d072cdb
baldzar@gmail.com <baldzar@gmail.com@aa6cf039-9e87-f74f-3bf4-c4349377eac6>
2011-05-11 14:32:56 +0000

    * added ucv::baldzarika::contour * implemented ucv::baldzarika::canny::fetch_contour

---
+++ trunk/ucv/lib_ucv/include/baldzarika/ucv/contour.h
@@ -0,0 +1,52 @@
+#ifndef BALDZARIKA_UCV_CONTOUR_H
+#define BALDZARIKA_UCV_CONTOUR_H
+
+#include <baldzarika/ucv/point2.h>
+#include <baldzarika/ucv/vector.h>
+
+namespace baldzarika { namespace ucv {
+
+	template < typename T >
+	class contour
+	{
+	public:
+		typedef point2<T> point2_t;
+		typedef std::vector<point2_t> points_t;
+
+		static bool check_is_closed(points_t const &pts, T const &eps)
+		{
+			if(pts.size()>3)
+			{
+				float dist=vector<T,2>(pts.back()-pts.front()).length();
+				return (vector<T,2>(pts.back()-pts.front()).length()<=eps);
+			}
+			return false;
+		}
+
+		contour()
+			: m_is_closed(false)
+		{
+		}
+
+		contour(points_t const &pts, bool is_closed)
+			: m_is_closed(is_closed)
+			, m_points(pts)
+		{
+		}
+
+		contour(points_t const &pts, T const &eps)
+			: m_is_closed(check_is_closed(pts,eps))
+			, m_points(pts)
+		{
+			if(m_is_closed)
+				m_points.pop_back();
+		}
+
+		bool		m_is_closed;
+		points_t	m_points;
+	};
+
+} //namespace ucv
+} //namespace baldzarika
+
+#endif //BALDZARIKA_UCV_CONTOUR_H
