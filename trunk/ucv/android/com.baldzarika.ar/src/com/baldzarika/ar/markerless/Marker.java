abce4b9f661affc63d7bc9558d5887d926060d5a
baldzar@gmail.com <baldzar@gmail.com@aa6cf039-9e87-f74f-3bf4-c4349377eac6>
2011-06-13 14:53:07 +0000

    * reflecting changes from lib_ar

---
+++ trunk/ucv/android/com.baldzarika.ar/src/com/baldzarika/ar/markerless/Marker.java
@@ -0,0 +1,39 @@
+package com.baldzarika.ar.markerless;
+
+import com.baldzarika.ar.Size2;
+
+public class Marker
+{
+	static { System.loadLibrary("com.baldzarika.ar"); }
+	
+	private native void create();
+	private native void create(Size2 ms);
+	private native void destroy();
+		
+	public Marker()
+	{
+		create();
+	}
+
+	public Marker(Size2 ms)
+	{
+		create(ms);
+	}
+	
+	public void dispose()
+	{
+		destroy();
+	}
+	
+	protected void finalize()
+	{
+		destroy();
+	}
+	
+	public native Size2 getSize();
+	public native boolean loadImage(String fileName);
+	public native boolean setImage(android.graphics.Bitmap bitmap);
+	public native boolean saveImage(String fileName);
+		
+	private long m_px=0;
+}
