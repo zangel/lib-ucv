03519ac1964840f9cf28c6a7aa0d23cad767c8b5
baldzar@gmail.com <baldzar@gmail.com@aa6cf039-9e87-f74f-3bf4-c4349377eac6>
2011-06-15 13:09:39 +0000

    * initial fiducial and markerless tracker tests

--- trunk/ucv/android/com.baldzarika.ar.test.App/src/com/baldzarika/ar/test/ArTestAppActivity.java
+++ trunk/ucv/android/com.baldzarika.ar.test.App/src/com/baldzarika/ar/test/ArTestAppActivity.java
@@ -8,6 +8,5 @@ public class ArTestAppActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
     }
 }
