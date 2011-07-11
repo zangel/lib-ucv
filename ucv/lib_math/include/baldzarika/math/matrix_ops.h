8149c611124f294b7180d7365cb030b303791579
baldzar <baldzar@gmail.com>
2011-07-11 23:29:37 +0000

    * some math::matrix fixes

--- ucv/lib_math/include/baldzarika/math/matrix_ops.h
+++ ucv/lib_math/include/baldzarika/math/matrix_ops.h
@@ -103,6 +103,7 @@ namespace baldzarika { namespace math {
 			for(boost::uint32_t r=0;r<R;++r)
 				for(boost::uint32_t c=0;c<C;++c)
 					lhs.m_data[r][c]*=static_cast<T>(s);
+			return lhs;
 		}
 
 		template < typename VT >
@@ -115,10 +116,8 @@ namespace baldzarika { namespace math {
 			return res;
 		}
 
-		template < typename RT, boost::uint32_t RR, boost::uint32_t RC > struct product_result;
-
-		template < typename RT, boost::uint32_t RC >
-		struct product_result<RT, C, RC>
+		template < typename RT, boost::uint32_t RR, boost::uint32_t RC >
+		struct product_result
 		{
 			typedef matrix<T, R, RC> type;
 		};
@@ -134,9 +133,10 @@ namespace baldzarika { namespace math {
 			{
 				for(boost::uint32_t c=0;c<result_t::COLS;++c)
 				{
-					res.m_data[r][c]=constant::zero<T>();
-					for(boost::uint32_t c2=0;c2<C;++c)
-						res.m_data[r][c]+=lhs.m_data[r][c2]*rhs.m_data[c2][c];
+					T res_value=constant::zero<T>();
+					for(boost::uint32_t c2=0;c2<C;++c2)
+						res_value+=lhs.m_data[r][c2]*rhs.m_data[c2][c];
+					res.m_data[r][c]=res_value;
 				}
 			}
 			return res;
@@ -162,8 +162,150 @@ namespace baldzarika { namespace math {
 			BOOST_ASSERT(false);
 			return matrix<T,C,R>();
 		}
-
 	};
+
+	template < typename T >
+	struct matrix_ops<T, 1, 1>
+	{
+		static inline matrix<T,1,1> const& zero()
+		{
+			static T const _zero[1][1]=
+			{
+				{ constant::zero<T>() }
+			};
+			return *(reinterpret_cast<matrix<T,1,1> const *>(&_zero[0][0]));
+		}
+
+		static inline matrix<T,1,1> const& identity()
+		{
+			static T const _identity[1][1]=
+			{
+				{ constant::one<T>()}
+			};
+			return *(reinterpret_cast<matrix<T,1,1> const *>(&_identity[0][0]));
+		}
+
+		static inline vector<T,1> get_row(matrix<T,1,1> const &mat, boost::int32_t r)
+		{
+			BOOST_ASSERT(r==0);
+			vector<T,1> row_vec;
+			row_vec.m_data[0]=mat.m_data[0][0];
+			return row_vec;
+		}
+
+		static inline vector<T,1> get_col(matrix<T,1,1> const &mat, boost::int32_t c)
+		{
+			BOOST_ASSERT(c==0);
+			vector<T,1> col_vec;
+			col_vec.m_data[0]=mat.m_data[0][0];
+			return col_vec;
+		}
+
+		template < typename RVT >
+		static inline matrix<T,1,1>& set_row(matrix<T,1,1> &mat, boost::int32_t r, vector<RVT,1> const &row_vec)
+		{
+			BOOST_ASSERT(r==0);
+			mat.m_data[0][0]=static_cast<T>(row_vec.m_data[0]);
+			return mat;
+		}
+
+		template < typename CVT >
+		static inline matrix<T,1,1>& set_col(matrix<T,1,1> &mat, boost::int32_t c, vector<CVT,1> const &col_vec)
+		{
+			BOOST_ASSERT(c==0);
+			mat.m_data[0][0]=static_cast<T>(col_vec.m_data[0]);
+			return mat;
+		}
+
+		template < typename RT >
+		static inline matrix<T,1,1>& assign(matrix<T,1,1> &lhs, matrix<RT,1,1> const &rhs)
+		{
+			lhs.m_data[0][0]=static_cast<T>(rhs.m_data[0][0]);
+			return lhs;
+		}
+
+		template < typename RT >
+		static inline matrix<T,1,1>& plus_assign(matrix<T,1,1> &lhs, matrix<RT,1,1> const &rhs)
+		{
+			lhs.m_data[0][0]+=static_cast<T>(rhs.m_data[0][0]);
+			return lhs;
+		}
+
+		template < typename RT >
+		static inline matrix<T,1,1>& minus_assign(matrix<T,1,1> &lhs, matrix<RT,1,1> const &rhs)
+		{
+			lhs.m_data[0][0]-=static_cast<T>(rhs.m_data[0][0]);
+			return lhs;
+		}
+
+		template < typename VT >
+		static inline matrix<T,1,1>& scalar_multiply_assign(matrix<T,1,1> &lhs, VT const &v)
+		{
+			lhs.m_data[0][0]*=static_cast<T>(v);
+			return lhs;
+		}
+
+		template < typename VT >
+		static inline vector<VT,1> vector_multiply(matrix<T,1,1> const &mat, vector<VT,1> const &vec)
+		{
+			vector<VT,1> res;
+			res.m_data[0]=static_cast<VT>(mat.m_data[0][0]*static_cast<T>(vec.m_data[0]));
+			return res;
+		}
+
+		template < typename RT, boost::uint32_t RR, boost::uint32_t RC > struct product_result;
+
+		template < typename RT, boost::uint32_t RC >
+		struct product_result<RT, 1, RC>
+		{
+			typedef matrix<T, 1, RC> type;
+		};
+
+		template < typename RT, boost::uint32_t RC >
+		static inline typename product_result<RT,1,RC>::type product(matrix<T,1,1> const &lhs, matrix<RT,1,RC> const &rhs)
+		{
+			typedef product_result<RT,1,RC>::type result_t;
+			result_t res;
+			for(boost::uint32_t c=0;c<result_t::COLS;++c)
+				res.m_data[0][c]=lhs.m_data[0][0]*rhs.m_data[0][c];
+			return res;
+		}
+
+
+		template < typename RT >
+		struct product_result<RT, 1, 1>
+		{
+			typedef matrix<T, 1, 1> type;
+		};
+
+		template < typename RT >
+		static inline matrix<T,1,1> product(matrix<T,1,1> const &lhs, matrix<RT,1,1> const &rhs)
+		{
+			matrix<T,1,1> res;
+			res.m_data[0][0]=lhs.m_data[0][0]*static_cast<T>(rhs.m_data[0][0]);
+			return res;
+		}
+
+		static inline matrix<T,1,1> transpose(matrix<T,1,1> const &mat)
+		{
+			return mat;
+		}
+
+		static inline T determinant(matrix<T,1,1> const &mat)
+		{
+			return mat.m_data[0][0];
+		}
+
+		static inline matrix<T,1,1> invert(matrix<T,1,1> const &mat)
+		{
+			matrix<T,1,1> res;
+			res.m_data[0][0]=std::abs(mat.m_data[0][0])>std::numeric_limits<T>::epsilon()?
+				math::constant::one<T>()/mat.m_data[0][0]:
+				math::constant::zero<T>();
+			return res;
+		}
+	};
+
 
 
 	template < typename T >
@@ -258,6 +400,7 @@ namespace baldzarika { namespace math {
 			lhs.m_data[0][1]*=static_cast<T>(v);
 			lhs.m_data[1][0]*=static_cast<T>(v);
 			lhs.m_data[1][1]*=static_cast<T>(v);
+			return lhs;
 		}
 
 		template < typename VT >
@@ -270,6 +413,27 @@ namespace baldzarika { namespace math {
 		}
 
 		template < typename RT, boost::uint32_t RR, boost::uint32_t RC > struct product_result;
+
+		template < typename RT, boost::uint32_t RC >
+		struct product_result<RT, 2, RC>
+		{
+			typedef matrix<T, 2, RC> type;
+		};
+
+		template < typename RT, boost::uint32_t RC >
+		static inline typename product_result<RT,2,RC>::type product(matrix<T,2,2> const &lhs, matrix<RT,2,RC> const &rhs)
+		{
+			typedef product_result<RT,2,RC>::type result_t;
+			result_t res;
+			for(boost::uint32_t c=0;c<result_t::COLS;++c)
+				res.m_data[0][c]=lhs.m_data[0][0]*rhs.m_data[0][c]+lhs.m_data[0][1]*rhs.m_data[1][c];
+
+			for(boost::uint32_t c=0;c<result_t::COLS;++c)
+				res.m_data[1][c]=lhs.m_data[1][0]*rhs.m_data[0][c]+lhs.m_data[1][1]*rhs.m_data[1][c];
+
+			return res;
+		}
+
 
 		template < typename RT >
 		struct product_result<RT, 2, 2>
