9f3ec2cce1bbeddd1cef3fe6d117dbb504384e8c
baldzar <baldzar@gmail.com>
2011-08-25 14:58:58 +0000

    * baldzarika::ucv::surf testing ...

--- ucv/lib_ucv/include/baldzarika/ucv/surf/describer.h
+++ ucv/lib_ucv/include/baldzarika/ucv/surf/describer.h
@@ -6,25 +6,39 @@
 
 namespace baldzarika { namespace ucv { namespace surf {
 
-	namespace detail {
+	template < typename T, boost::uint32_t NB, boost::uint32_t NSB >
+	class describer
+	{
+	public:
+		typedef feature_point<T, NB> feature_point_t;
 
-		template < typename T , boost::uint32_t NB, boost::uint32_t NSB >
+		typedef typename gil::pixel<T, gil::gray_layout_t> win_pixel_t;
+		typedef typename gil::image< win_pixel_t, false, std::allocator<unsigned char> > win_image_t;
+		typedef typename win_image_t::view_t win_view_t;
+		typedef typename win_image_t::const_view_t win_const_view_t;
+
+		static boost::uint32_t const PATCH_SIZE=NB*NSB;
+	private:
+
+		struct win_sample
+		{
+			boost::uint32_t m_win;
+			boost::uint32_t m_patch;
+			T				m_wt;
+		};
+
 		class describer_helper
 		{
 		public:
-			static boost::uint32_t const KERNEL_SIZE=NB*NSB;
+			
 		private:
 			describer_helper()
 			{
-				T g_k[KERNEL_SIZE];
-				make_gaussian_kernel(g_k, T(3.3));
-				for(boost::uint32_t y=0;y<KERNEL_SIZE;++y)
-				{
-					for(boost::uint32_t x=0;x<KERNEL_SIZE;++x)
-					{
+				T g_k[PATCH_SIZE];
+				make_gaussian_kernel(g_k, T(3.3), false);
+				for(boost::uint32_t y=0;y<PATCH_SIZE;++y)
+					for(boost::uint32_t x=0;x<PATCH_SIZE;++x)
 						m_gauss33[y][x]=g_k[y]*g_k[x];
-					}
-				}
 			}
 
 		public:
@@ -34,27 +48,15 @@ namespace baldzarika { namespace ucv { namespace surf {
 				return _describer_helper;
 			}
 		public:
-			T		m_gauss33[KERNEL_SIZE][KERNEL_SIZE];
+			T		m_gauss33[PATCH_SIZE][PATCH_SIZE];
 		};
 
-
-	} //namespace detail
-
-	template < typename T, boost::uint32_t NB, boost::uint32_t NSB >
-	class describer
-	{
 	public:
 
-		typedef feature_point<T, NB> feature_point_t;
-		typedef detail::describer_helper<T,NB,NSB> describer_helper_t;
-
-		typedef typename gil::pixel<T, gil::gray_layout_t> win_pixel_t;
-		typedef typename gil::image< win_pixel_t, false, std::allocator<unsigned char> > win_image_t;
-		typedef typename win_image_t::view_t win_view_t;
-		typedef typename win_image_t::const_view_t win_const_view_t;
-
+		
 
 		describer()
+			: m_win_samples_size(0)
 		{
 		}
 
@@ -64,20 +66,63 @@ namespace baldzarika { namespace ucv { namespace surf {
 			typedef typename IVT::value_type	iv_pixel_t;
 			typedef typename gil::channel_type<iv_pixel_t>::type iv_channel_t;
 
-			boost::int32_t fp_scale=fp.m_scale;
-			T s=static_cast<T>(fp_scale*1.2f/9.0f);
-			boost::int32_t win_size=math::iround(T(describer_helper_t::KERNEL_SIZE+1)*s);
-			BOOST_ASSERT(m_win_image.width()==m_win_image.height());
-			if(m_win_image.width()<win_size+1) m_win_image.recreate(win_size+1,win_size+1);
+			static describer_helper const &desc_helper=describer_helper::get();
 
-			win_view_t win_view=gil::subimage_view(
-				gil::view(m_win_image),
-				0, 0,
-				win_size+1, win_size+1
-			);
+			T s=static_cast<T>(fp.m_scale*1.2f/9.0f);
+			T win_ss=math::constant::one<T>()/s;
+			T win_sa=win_ss*win_ss;
 
+			BOOST_ASSERT(win_ss<math::constant::one<T>());
+			
+			T patch_real_size=T(PATCH_SIZE+1);
+			T win_real_size=T(PATCH_SIZE+1)*s;
+			
+			boost::uint32_t win_size=std::ceil(win_real_size);
+			boost::uint32_t win_samples_size=win_size+PATCH_SIZE+1;
+			
+			if(m_win_samples_size<win_samples_size)
+			{
+				m_win_samples.reset( new win_sample[win_samples_size] );
+				m_win_samples_size=win_samples_size;
+			}
+
+			win_sample *pwin_samples=m_win_samples.get();
+			
+			boost::uint32_t win_samples_count=0;
+			for(boost::uint32_t iw=0;iw<win_size;++iw)
+			{
+				T ps1=T(iw)*win_ss;
+				T ps2=T(iw+1)*win_ss;
+				boost::uint32_t ips1=static_cast<boost::uint32_t>(ps1);
+				boost::uint32_t ips2=static_cast<boost::uint32_t>(ps2);
+
+				if(ips1<ips2)
+				{
+					pwin_samples[win_samples_count].m_win=iw;
+					pwin_samples[win_samples_count].m_patch=ips1;
+					pwin_samples[win_samples_count++].m_wt=(T(ips2)-ps1)*s;
+					if(ips2<PATCH_SIZE+1)
+					{
+						pwin_samples[win_samples_count].m_win=iw;
+						pwin_samples[win_samples_count].m_patch=ips2;
+						pwin_samples[win_samples_count++].m_wt=(ps2-T(ips2))*s;
+					}
+				}
+				else
+				{
+					pwin_samples[win_samples_count].m_win=iw;
+					pwin_samples[win_samples_count].m_patch=ips1;
+					pwin_samples[win_samples_count++].m_wt=math::constant::one<T>();
+				}
+			}
+
+			for(boost::uint32_t y=0;y<PATCH_SIZE+1;++y)
+				for(boost::uint32_t x=0;x<PATCH_SIZE+1;++x)
+					m_patch[y][x]=math::constant::zero<T>();
+			
+						
 			T sin_ori=std::sin(fp.m_orientation);
-            T cos_ori=std::cos(fp.m_orientation) ;
+            T cos_ori=std::cos(fp.m_orientation);
             
 			T win_offset=-T(win_size-1)*math::constant::half<T>();
 
@@ -85,194 +130,77 @@ namespace baldzarika { namespace ucv { namespace surf {
 			T start_y=fp.y()-win_offset*sin_ori+win_offset*cos_ori;
 
 
-			//build integral window
-			T *win_row=reinterpret_cast<T*>(win_view.row_begin(0));
-
-			for(boost::int32_t j=0;j<=win_size;++j)
-				win_row[j]=math::constant::zero<T>();
-
-			win_row++;
-
-			for(boost::int32_t i=0;i<win_size;++i,start_x+=sin_ori,start_y+=cos_ori)
+			
+			for(boost::uint32_t ys=0;ys<win_samples_count;start_x+=sin_ori,start_y+=cos_ori)
             {
+				for(boost::uint32_t i=0;i<PATCH_SIZE+1;++i)
+					m_patch_buff[i]=math::constant::zero<T>();
+
                 T pixel_x=start_x;
 				T pixel_y=start_y;
 				
-				T *prev_win_row=win_row;
-				win_row=reinterpret_cast<T*>(win_view.row_begin(i+1))+1;
-
-				T sum=win_row[-1]=math::constant::zero<T>();
-								
-				for(boost::int32_t j=0;j<win_size;j++,pixel_x+=cos_ori,pixel_y-=sin_ori)
+				for(boost::uint32_t xs=0;xs<win_samples_count;pixel_x+=cos_ori,pixel_y-=sin_ori)
                 {
 					boost::int32_t x=std::min(std::max(math::iround(pixel_x),0),iv.width()-1);
                     boost::int32_t y=std::min(std::max(math::iround(pixel_y),0),iv.height()-1);
-					win_row[j]=prev_win_row[j]+(sum+=T(*(reinterpret_cast<iv_channel_t const *>(iv.row_begin(y))+x)));
-					//win_row[j]=T(*(reinterpret_cast<iv_channel_t const *>(iv.row_begin(y))+x));
+					T win_sample=T(*(reinterpret_cast<iv_channel_t const *>(iv.row_begin(y))+x))*win_sa;
+
+					boost::uint32_t xwin=pwin_samples[xs].m_win;
+					while(xs<win_samples_count && pwin_samples[xs].m_win==xwin)
+					{
+						m_patch_buff[pwin_samples[xs].m_patch]+=win_sample*pwin_samples[xs].m_wt;
+						xs++;
+					}
+				}
+
+				boost::uint32_t ywin=pwin_samples[ys].m_win;
+				while(ys<win_samples_count && pwin_samples[ys].m_win==ywin)
+				{
+					for(boost::uint32_t p=0;p<PATCH_SIZE+1;++p)
+						m_patch[pwin_samples[ys].m_patch][p]+=m_patch_buff[p]*pwin_samples[ys].m_wt;
+					ys++;
 				}
             }
-
 #if 0
-			{
-				ucv::gil::gray8_image_t save_img(win_view.width(),win_view.height());
-				ucv::convert(
-					win_view,
-					ucv::gil::view(save_img),
-					ucv::detail::grayscale_convert()
-					);
-
-				ucv::gil::png_write_view("win_view.png", ucv::gil::const_view(save_img));
-			}
-#endif			
-			
-
-			//compute gradients using integral window
-			for(boost::uint32_t i=0;i<describer_helper_t::KERNEL_SIZE;++i)
-			{
-				for(boost::uint32_t j=0;j<describer_helper_t::KERNEL_SIZE;++j)
-				{
-					m_dx[i][j]=m_dy[i][j]=math::constant::zero<T>();
-				}
-			}
-
-			T win_scale=T(win_size)/T(describer_helper_t::KERNEL_SIZE+1);
-			boost::int32_t ww_step=win_view.row_begin(1)-win_view.row_begin(0);
-			integral_box<T> ib=
-				integral_box<T>(
-					math::point2i(0,0),
-					math::size2i(1,1),
-					ww_step,
-					math::constant::one<T>()
-				).scaled(win_scale,ww_step);
-
-			
-			T const *cwin_row=reinterpret_cast<T const *>(win_view.row_begin(0));
-			//top left
-			T w_tl=integral_sample<T,T>(
-				cwin_row,
-				ib
-			);
-			m_dx[0][0]-=w_tl;
-			m_dy[0][0]-=w_tl;
-			
-			//top row
-			T wx=win_scale;
-			for(boost::uint32_t x=0;x<describer_helper_t::KERNEL_SIZE;++x, wx+=win_scale)
-			{
-				T w=integral_sample<T,T>(
-					cwin_row+math::iround(wx),
-					ib
-				);
-				m_dx[0][x-1]+=w;
-				m_dx[0][x]-=w;
-				m_dy[0][x-1]-=w;
-				m_dy[0][x]-=w;
-			}
-
-			//top right
-			T w_tr=integral_sample<T,T>(
-				reinterpret_cast<T const *>(cwin_row+math::iround(T(describer_helper_t::KERNEL_SIZE)*win_scale)),
-				ib
-			);
-			m_dx[0][describer_helper_t::KERNEL_SIZE-1]+=w_tr;
-			m_dy[0][describer_helper_t::KERNEL_SIZE-1]-=w_tr;
-
-			T wy=win_scale;
-			for(boost::uint32_t y=1;y<describer_helper_t::KERNEL_SIZE;++y, wy+=win_scale)
-			{
-				cwin_row=reinterpret_cast<T const *>(win_view.row_begin(math::iround(wy)));
-
-				//left
-				T w_l=integral_sample<T,T>(
-					cwin_row,
-					ib
-				);
-				
-				m_dx[y-1][0]-=w_l;
-				m_dx[y][0]-=w_l;
-				m_dy[y-1][0]+=w_l;
-				m_dy[y][0]-=w_l;
-
-				wx=win_scale;
-				for(boost::uint32_t x=1;x<describer_helper_t::KERNEL_SIZE;++x, wx+=win_scale)
-				{
-					T w=integral_sample<T,T>(
-						cwin_row+math::iround(wx),
-						ib
-					);
-					m_dx[y-1][x-1]+=w;
-					m_dx[y-1][x]-=w;
-					m_dx[y][x-1]+=w;
-					m_dx[y][x]-=w;
-
-					m_dy[y-1][x-1]+=w;
-					m_dy[y-1][x]+=w;
-					m_dy[y][x-1]-=w;
-					m_dy[y][x]-=w;
-				}
-
-				//right
-				T w_r=integral_sample<T,T>(
-					cwin_row+math::iround(T(describer_helper_t::KERNEL_SIZE)*win_scale),
-					ib
-				);
-				m_dx[y-1][describer_helper_t::KERNEL_SIZE-1]+=w_r;
-				m_dx[y][describer_helper_t::KERNEL_SIZE-1]+=w_r;
-
-				m_dy[y-1][describer_helper_t::KERNEL_SIZE-1]+=w_r;
-				m_dy[y][describer_helper_t::KERNEL_SIZE-1]-=w_r;
-			}
-
-			cwin_row=reinterpret_cast<T const *>(win_view.row_begin(math::iround(T(describer_helper_t::KERNEL_SIZE)*win_scale)));
-
-			//bottom left
-			T w_bl=integral_sample<T,T>(
-				cwin_row,
-				ib
-			);
-			m_dx[describer_helper_t::KERNEL_SIZE-1][0]-=w_bl;
-			m_dy[describer_helper_t::KERNEL_SIZE-1][0]+=w_bl;
-			
-			//bottom row
-			wx=win_scale;
-			for(boost::uint32_t x=1;x<describer_helper_t::KERNEL_SIZE;++x, wx+=win_scale)
-			{
-				T w=integral_sample<T,T>(
-					cwin_row+math::iround(wx),
-					ib
-				);
-
-				m_dx[describer_helper_t::KERNEL_SIZE-1][x-1]+=w;
-				m_dx[describer_helper_t::KERNEL_SIZE-1][x]-=w;
-
-				m_dy[describer_helper_t::KERNEL_SIZE-1][x-1]+=w;
-				m_dy[describer_helper_t::KERNEL_SIZE-1][x]+=w;
-			}
-
-
-			//bottom right
-			T w_br=integral_sample<T,T>(
-				cwin_row+math::iround(T(describer_helper_t::KERNEL_SIZE)*win_scale),
-				ib
-			);
-			m_dx[describer_helper_t::KERNEL_SIZE-1][describer_helper_t::KERNEL_SIZE-1]+=w_br;
-			m_dy[describer_helper_t::KERNEL_SIZE-1][describer_helper_t::KERNEL_SIZE-1]+=w_br;
-
-#if 0
-			for(boost::uint32_t i=0;i<describer_helper_t::KERNEL_SIZE;++i)
-			{
-				for(boost::uint32_t j=0;j<describer_helper_t::KERNEL_SIZE;++j)
-				{
-					m_dx[i][j]=0.5f*(1.0f+m_dx[i][j]);
-					m_dy[i][j]=0.5f*(1.0f+m_dy[i][j]);
-				}
-			}
-			ucv::gil::gray8_image_t grad_img(describer_helper_t::KERNEL_SIZE,describer_helper_t::KERNEL_SIZE);
+			ucv::gil::gray8_image_t patch_img(PATCH_SIZE+1,PATCH_SIZE+1);
 			ucv::convert(
 				ucv::gil::interleaved_view(
-					describer_helper_t::KERNEL_SIZE, describer_helper_t::KERNEL_SIZE,
+					PATCH_SIZE+1, PATCH_SIZE+1,
+					reinterpret_cast<win_pixel_t const *>(&m_patch[0][0]),
+					(PATCH_SIZE+1)*sizeof(T)
+				),
+				ucv::gil::view(patch_img),
+				ucv::detail::grayscale_convert()
+			);
+			ucv::gil::png_write_view("patch.png", ucv::gil::const_view(patch_img));
+#endif
+
+			for(boost::uint32_t y=0;y<PATCH_SIZE;++y)
+			{
+				for(boost::uint32_t x=0;x<PATCH_SIZE;++x)
+                {
+                    T gw=desc_helper.m_gauss33[y][x];
+					m_dx[y][x]=(m_patch[y][x+1]-m_patch[y][x]+m_patch[y+1][x+1]-m_patch[y+1][x])*gw;
+					m_dy[y][x]=(m_patch[y+1][x]-m_patch[y][x]+m_patch[y+1][x+1]-m_patch[y][x+1])*gw;
+                }
+			}
+
+
+#if 0
+			for(boost::uint32_t i=0;i<PATCH_SIZE;++i)
+			{
+				for(boost::uint32_t j=0;j<PATCH_SIZE;++j)
+				{
+					m_dx[i][j]=0.5f*(1.0f+50.0f*m_dx[i][j]);
+					m_dy[i][j]=0.5f*(1.0f+50.0f*m_dy[i][j]);
+				}
+			}
+			ucv::gil::gray8_image_t grad_img(PATCH_SIZE,PATCH_SIZE);
+			ucv::convert(
+				ucv::gil::interleaved_view(
+					PATCH_SIZE, PATCH_SIZE,
 					reinterpret_cast<win_pixel_t const *>(&m_dx[0][0]),
-					describer_helper_t::KERNEL_SIZE*sizeof(T)
+					PATCH_SIZE*sizeof(T)
 				),
 				ucv::gil::view(grad_img),
 				ucv::detail::grayscale_convert()
@@ -281,20 +209,16 @@ namespace baldzarika { namespace ucv { namespace surf {
 
 			ucv::convert(
 				ucv::gil::interleaved_view(
-					describer_helper_t::KERNEL_SIZE, describer_helper_t::KERNEL_SIZE,
+					PATCH_SIZE, PATCH_SIZE,
 					reinterpret_cast<win_pixel_t const *>(&m_dy[0][0]),
-					describer_helper_t::KERNEL_SIZE*sizeof(T)
+					PATCH_SIZE*sizeof(T)
 				),
 				ucv::gil::view(grad_img),
 				ucv::detail::grayscale_convert()
 			);
 			ucv::gil::png_write_view("dy.png", ucv::gil::const_view(grad_img));
 #endif
-			
-
-			
 			//compute the descriptor
-			static describer_helper_t const &desc_helper=describer_helper_t::get();
 			fp.m_descriptor=feature_point_t::descriptor_t::zero();
 			for(boost::uint32_t i=0;i<NB;++i)
 			{
@@ -306,8 +230,8 @@ namespace baldzarika { namespace ucv { namespace surf {
 					{
 						for(boost::uint32_t x=j*NSB;x<j*NSB+5;++x)
 						{
-							T tx=m_dx[y][x]*desc_helper.m_gauss33[y][x];
-							T ty=m_dy[y][x]*desc_helper.m_gauss33[y][x];
+							T tx=m_dx[y][x];
+							T ty=m_dy[y][x];
 							vec[0]+=tx;
 							vec[1]+=ty;
 							vec[2]+=std::abs(tx);
@@ -320,9 +244,13 @@ namespace baldzarika { namespace ucv { namespace surf {
 		}
 	
 	private:
-		mutable win_image_t		m_win_image;
-		mutable	T				m_dx[describer_helper_t::KERNEL_SIZE][describer_helper_t::KERNEL_SIZE];
-		mutable	T				m_dy[describer_helper_t::KERNEL_SIZE][describer_helper_t::KERNEL_SIZE];
+		mutable boost::scoped_array<win_sample>		m_win_samples;
+		mutable	boost::uint32_t						m_win_samples_size;
+
+		mutable	T									m_patch_buff[PATCH_SIZE+1];
+		mutable	T									m_patch[PATCH_SIZE+1][PATCH_SIZE+1];
+		mutable	T									m_dx[PATCH_SIZE][PATCH_SIZE];
+		mutable	T									m_dy[PATCH_SIZE][PATCH_SIZE];
 	};
 
 } //namespace surf
